da47a8fee08f39f6fe560c65e28b8f44e7ae9cb0
IamOppy <randolffretrato@yahoo.com>
2021-11-17 22:47:55 -0800

    Second File Commit

--- GUIpasswordsaver.py
+++ GUIpasswordsaver.py
@@ -2,30 +2,31 @@ import tkinter as tk
 import datetime
 import random
 import socket
-from tkinter import *
+from tkinter import font
 
 class Application():
     def __init__(self):
         window = tk.Tk()
         window.geometry("400x400")
         window.title("Zephyrus Database Saver")
-        self.Login_main(window)
+        Custom_Font = tk.font.Font(family='cambria', size=10)
+        self.Login_main(window, Custom_Font)
         window.mainloop()
-        Custom_Font = tk.Font()
+        
         
 
-    def Login_main(self, window):
-         USER_NAME_TEXT = Label(window, text='UserName')
+    def Login_main(self, window, Custom_Font):
+         USER_NAME_TEXT = tk.Label(window, text='UserName', font=Custom_Font)
          USER_NAME_TEXT.place(x=200, y=125, anchor='center')
-         USER_NAME_ENTRY = Entry(window, bd=3)
+         USER_NAME_ENTRY = tk.Entry(window, bd=3)
          USER_NAME_ENTRY.place(x=200, y=150, anchor="center")
          
-         PASSWORD_TEXT = Label(window, text='Password')
+         PASSWORD_TEXT = tk.Label(window, text='Password', font=Custom_Font)
          PASSWORD_TEXT.place(x=200, y=180, anchor='center')
-         PASSWORD_ENTRY = Entry(window, bd=3)
+         PASSWORD_ENTRY = tk.Entry(window, bd=3)
          PASSWORD_ENTRY.place(x=200, y=205, anchor='center')
 
-         LOGIN_BUTTON = Button(window, text='LOGIN')
+         LOGIN_BUTTON = tk.Button(window, text='LOGIN', font=Custom_Font)
          LOGIN_BUTTON.place(x=200, y=250, anchor='center')
         
        
