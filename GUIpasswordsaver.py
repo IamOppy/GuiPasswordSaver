091d6f36d75d7dd509067a73ca63fa03217e1368
IamOppy <randolffretrato@yahoo.com>
2021-11-18 23:58:35 -0800

    file up-to-date

--- GUIpasswordsaver.py
+++ GUIpasswordsaver.py
@@ -2,7 +2,10 @@ import tkinter as tk
 import datetime
 import random
 import socket
-from tkinter import font
+import sqlite3
+
+from tkinter import Message, font, messagebox
+
 
 class Application():
     def __init__(self):
@@ -10,25 +13,29 @@ class Application():
         window.geometry("400x400")
         window.title("Zephyrus Database Saver")
         Custom_Font = tk.font.Font(family='cambria', size=10)
-        self.Login_main(window, Custom_Font)
+
+        USER_NAME_TEXT = tk.Label(window, text='Username', font=Custom_Font)
+        USER_NAME_TEXT.place(x=200, y=125, anchor='center')
+        self.USER_NAME_ENTRY = tk.Entry(window, bd=3)
+        self.USER_NAME_ENTRY.place(x=200, y=150, anchor="center")
+
+        PASSWORD_TEXT = tk.Label(window, text='Password', font=Custom_Font)
+        PASSWORD_TEXT.place(x=200, y=180, anchor='center')
+        self.PASSWORD_ENTRY = tk.Entry(window, bd=3)
+        self.PASSWORD_ENTRY.place(x=200, y=205, anchor='center')
+        LOGIN_BUTTON = tk.Button(window, text='LOGIN', font=Custom_Font,
+                                 command=self.Check_Login_details)
+        LOGIN_BUTTON.place(x=200, y=250, anchor='center')
         window.mainloop()
-        
-        
 
-    def Login_main(self, window, Custom_Font):
-         USER_NAME_TEXT = tk.Label(window, text='UserName', font=Custom_Font)
-         USER_NAME_TEXT.place(x=200, y=125, anchor='center')
-         USER_NAME_ENTRY = tk.Entry(window, bd=3)
-         USER_NAME_ENTRY.place(x=200, y=150, anchor="center")
-         
-         PASSWORD_TEXT = tk.Label(window, text='Password', font=Custom_Font)
-         PASSWORD_TEXT.place(x=200, y=180, anchor='center')
-         PASSWORD_ENTRY = tk.Entry(window, bd=3)
-         PASSWORD_ENTRY.place(x=200, y=205, anchor='center')
+    def Check_Login_details(self):
+        username = self.USER_NAME_ENTRY.get()
+        password = self.PASSWORD_ENTRY.get()
 
-         LOGIN_BUTTON = tk.Button(window, text='LOGIN', font=Custom_Font)
-         LOGIN_BUTTON.place(x=200, y=250, anchor='center')
-        
-       
+    def _database(self):
+        conn = sqlite3.connect('011223054.db')
+        print('Opened Database')
+
+
 obj = Application()
 obj.main()
